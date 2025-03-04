385cc784533573683d946d5be019c80da615dea5
Rickfalton <rickonyach@gmail.com>
2025-03-04 10:45:13 +0300

    navbar

---
+++ my-app/src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Navbar.css'; // Import the CSS file for styling
+
+const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMobile(!isMobile);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <a href="/">RICKLALTON PORTFOLIO</a>
+      </div>
+      <ul className={`nav-links ${isMobile ? 'active' : ''}`}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">Projects</a></li>
+        <li><a href="#services">Stack</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="menu-icon" onClick={toggleMenu}>
+        &#9776;
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
