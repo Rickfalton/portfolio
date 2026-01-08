6e66dd165c9f9dfbe78e79bb7bad439f52ac2580
Rickfalton <rickonyach@gmail.com>
2026-01-08 17:55:24 +0300

    contact email

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
             <h2>Contact Me</h2>
             <form>
                 <input type="text" placeholder="RICKFALTON ODHIAMBO" required />
-                <input type="email" placeholder="rickonyach@gmail.com" required />
+                <input type="email" placeholder="forsmanrick@gmail.com" required />
                 <textarea placeholder="Your Message" required></textarea>
                 <button type="submit">Send</button>
             </form>
